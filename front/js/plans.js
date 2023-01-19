b7f59f80a04de707f37bfb2e668d57290040e516
Benoit62 <65405337+Benoit62@users.noreply.github.com>
2023-01-19 10:07:39 +0100

    Code corentin

--- front/js/plans.js
+++ front/js/plans.js
@@ -1,6 +1,7 @@
 // Constantes
 const addBtn = document.querySelector(".add");
 const input = document.querySelector(".inp-group");
+const edit = document.querySelector(".editing");
 
 // Fonctions
 
@@ -8,29 +9,40 @@ function removeInput(){
     this.parentElement.remove();
 }
 
+function redirectEdit(){
+    document.location.href="../html/reservation.html";
+}
+
 function addInput(){
     const name = document.createElement("input");
     name.type="text";
     name.placeholder = "Enter Your Name";
 
-    const email = document.createElement("input");
-    email.type="email";
-    email.placeholder="Enter your Email";
+    const edits= document.createElement("a");
+    edits.className = "editer";
+    edits.innerHTML = "<img src='../assets/editer.png' width='15px' height='15px'>";
 
     const btn = document.createElement("a");
     btn.className = "delete";
     btn.innerHTML = "&times";
 
     btn.addEventListener("click", removeInput);
+    edits.addEventListener("click", redirectEdit);
 
     const flex = document.createElement("div");
     flex.className = "flex";
 
     input.appendChild(flex);
     flex.appendChild(name);
-    flex.appendChild(email);
     flex.appendChild(btn);
+    flex.appendChild(edits);
+}
+
+function edition(){
+    document.location.href="../html/reservation.html";
 }
 
 
 addBtn.addEventListener("click", addInput);
+
+edit.addEventListener("click", edition);
