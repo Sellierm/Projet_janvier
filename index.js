7f6b3e2c2a60a391f59f0f5e72f2e9b632bde70d
Mathieu <Mathieu.sellier@student.junia.com>
2023-01-20 00:27:36 +0100

    supression route inutile

--- index.js
+++ index.js
@@ -50,10 +50,6 @@ app.get('/', function (req, res) {
     });
     res.sendFile(path.join(__dirname + '/front/html/login.html'));
 });
-//register page
-app.get('/reg', function (req, res) {
-    res.sendFile(path.join(__dirname + '/front/html/register.html'));
-});
 //home page
 app.get('/home', authMiddleware, function (req, res) {
     if (req.session.isAdmin) {
