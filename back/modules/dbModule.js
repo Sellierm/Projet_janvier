69f5d097fed2a6902f2410686d70d0cece76da16
Mathieu <Mathieu.sellier@student.junia.com>
2023-01-19 16:28:18 +0100

    change mongodb

--- back/modules/dbModule.js
+++ back/modules/dbModule.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://10.224.3.201:27017/',
+    url: 'mongodb://192.168.19.73:27017/',
     name: 'Projet_janvier',
     database_users: 'Users',
     database_rooms: 'Rooms',
